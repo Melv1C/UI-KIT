613be075c2c81d3ce16d2e1f8e536362bd14206f
Melv1C <melvyn.claes@student.uclouvain.be>
2024-09-17 14:09:27 +0200

    navbar

--- src/UI/NavBar/NavBar.tsx
+++ src/UI/NavBar/NavBar.tsx
@@ -1,39 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import './NavBar.css'; 
+import React, { useEffect, useState } from "react";
+import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 export type NavLinks = {
-    name: string;
-    link: string;
-    icon?: string;
-}
+  name: string;
+  link: string;
+  icon?: string;
+};
 
 export type NavBarProps = {
-    title: string;
-    navLinks: NavLinks[];
-}
+  title: string;
+  navLinks: NavLinks[];
+  reactRouter?: boolean;
+};
 
 export const NavBar: React.FC<NavBarProps> = ({
-    title, 
-    navLinks
+  title,
+  navLinks,
+  reactRouter = false,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false)
-
-    return (
-        <nav className="kit-navbar">
-            <div className="kit-navbar-title">{title}</div>
-            <ul className={`kit-navbar-links ${isOpen ? 'open' : ''}`}>
-                {navLinks.map((link, index) => (
-                    <li key={index} className="kit-navbar-link">
-                        <a href={link.link}>{link.name}</a>
-                    </li>
-                ))}
-            </ul>
-            <div className="kit-navbar-burger" onClick={() => setIsOpen(prev => !prev)}>
-                <div className="kit-navbar-burger-line"></div>
-                <div className="kit-navbar-burger-line"></div>
-                <div className="kit-navbar-burger-line"></div>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="kit-navbar">
+      <div className="kit-navbar-title">{title}</div>
+      <ul className={`kit-navbar-links ${isOpen ? "open" : ""}`}>
+        {navLinks.map((link, index) => (
+          <li key={index} className="kit-navbar-link">
+            {reactRouter ? (
+              <Link to={link.link}>{link.name}</Link>
+            ) : (
+              <a href={link.link}>{link.name}</a>
+            )}
+          </li>
+        ))}
+      </ul>
+      <div
+        className="kit-navbar-burger"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <div className="kit-navbar-burger-line"></div>
+        <div className="kit-navbar-burger-line"></div>
+        <div className="kit-navbar-burger-line"></div>
+      </div>
+    </nav>
+  );
+};
